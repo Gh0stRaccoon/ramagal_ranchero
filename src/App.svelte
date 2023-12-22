7edccd85c399124d8c1ec5b9d1ca356905b90d86
Joaquín Ignacio Ossandón Gómez <joaquinossandong@gmail.com>
2023-12-22 19:06:46 -0300

    🚀 Starting proyect

---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+  import video from "./assets/ramagal.webp";
+  import Logo from "./assets/Logo.png";
+  import Nav from "./components/Nav.svelte";
+</script>
+
+<main>
+  <Nav />
+  <header>
+    <img class="video" src={video} alt="" />
+    <div class="overlay"></div>
+    <img class="logo" src={Logo} alt="" />
+  </header>
+</main>
+
+<style>
+  header {
+    position: relative;
+    height: 100vh;
+    overflow: hidden;
+  }
+  .video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: left;
+    filter: contrast(1.2) brightness(0.8);
+    position: inherit;
+  }
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #33333377;
+    mix-blend-mode: hard-light;
+  }
+  .logo {
+    width: 70%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    animation:
+      fade-in 2.5s ease-in-out forwards,
+      bump 4s ease-in-out infinite 2.5s;
+  }
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+      transform: translate(-50%, -50%) scale(0.5);
+    }
+    80% {
+      opacity: 1;
+      transform: translate(-50%, -50%) scale(1.2);
+    }
+    90% {
+      opacity: 1;
+      transform: translate(-50%, -50%) scale(1);
+    }
+  }
+
+  @keyframes bump {
+    0% {
+      transform: translate(-50%, -50%) scale(1);
+    }
+    50% {
+      transform: translate(-50%, -50%) scale(1.2);
+    }
+    100% {
+      transform: translate(-50%, -50%) scale(1);
+    }
+  }
+
+  @media (width > 768px) {
+    .logo {
+      width: 30%;
+    }
+  }
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,120 @@
+<script>
+  import { onDestroy, onMount } from "svelte";
+  import MenuIcon from "../assets/icons/menu.svelte";
+  let menuOpen = false;
+
+  const handleClickOutside = (event) => {
+    if (event.target.closest(".menu") || event.target.closest(".menu-button")) {
+      return;
+    }
+    menuOpen = false;
+  };
+
+  onDestroy(() => {
+    window.removeEventListener("click", handleClickOutside);
+  });
+
+  onMount(() => {
+    window.addEventListener("click", handleClickOutside);
+  });
+</script>
+
+<nav>
+  <div class="text-logo">
+    <h1>RAMAGAL</h1>
+  </div>
+  <button
+    class="menu-button"
+    on:click|stopPropagation={() => (menuOpen = !menuOpen)}
+  >
+    <MenuIcon height={40} width={40} />
+  </button>
+  <div class="menu" class:active={menuOpen}>
+    <ul>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Servicios</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+  </div>
+</nav>
+
+<style>
+  nav {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0 2rem;
+    height: 10vh;
+    background: transparent;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+  }
+  .text-logo {
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: #ffffff99;
+    -webkit-text-stroke: 1px #705638;
+    filter: drop-shadow(0 0 4px #e1be95);
+    mix-blend-mode: color;
+    font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  }
+  .text-logo h1 {
+    margin: 0;
+  }
+  .menu-button {
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: #dcdcdc;
+    background: none;
+    border: none;
+    display: flex;
+    padding: 0;
+  }
+  .menu {
+    position: absolute;
+    top: 0;
+    left: -100%;
+    transition: left 0.25s ease-in-out;
+    width: 50%;
+  }
+
+  .menu:global(.active) {
+    left: 0;
+  }
+
+  .menu ul {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  .menu ul li a {
+    font-size: 1rem;
+    font-weight: 700;
+    color: #ffffff;
+    text-decoration: none;
+    text-transform: uppercase;
+    transition: all 0.25s ease-in-out;
+    padding: 1rem 2rem;
+    background-color: #705638;
+    width: 100%;
+    display: block;
+  }
+  .menu ul li a:hover {
+    color: #e1be95;
+  }
+
+  @media (width > 768px) {
+    .menu-button {
+      display: none;
+    }
+    .menu {
+      display: block;
+    }
+  }
+</style>
